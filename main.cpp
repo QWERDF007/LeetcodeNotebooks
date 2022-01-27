4a2078b921722e4de89bbf31adc5b68ef1d3c6ba
wangtao <595517713@qq.com>
2022-01-27 18:10:06 +0800

    feat(main): 添加array题目入口

--- main.cpp
+++ main.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
+#include <string>
 
 #include <cxxopts.hpp>
 #include <benchmark/benchmark.h>
-#include <cpuinfo.h>
 
+#include "array/array.h"
+#include "leetcode_solution.h"
 
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state)
-        std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
-    for (auto _ : state)
-        std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
-
-//BENCHMARK_MAIN();
 
 int main(int argc, char* argv[])
 {
-    /*cpuinfo_initialize();
-    auto cpu_package = cpuinfo_get_package(0);
-    std::cout << "Running on %s CPU\n" << cpu_package->name << std::endl;
-    std::cout << "Cpu count: " << cpu_package->core_count << std::endl;*/
+    cxxopts::Options options(argv[0], "Leetcode C++ Solutions");
+    options.add_options()
+        ("t,type", "problem type", cxxopts::value<std::string>())
+        ("i,pid", "problem id", cxxopts::value<int>())
+        ("h,help", "Print usage");
+    auto opts = options.parse(argc, argv);
+    if (opts.count("help")) {
+        std::cout << options.help() << std::endl;
+        exit(EXIT_SUCCESS);
+    }
+
+    std::string type = "";
+    int pid = -1;
+    if (opts.count("type")) {
+        type = opts["type"].as<std::string>();
+    }
+    if (opts.count("pid")) {
+        pid = opts["pid"].as<int>();
+    }
+    if (type == "array") {
+        leetcode::array::ArraySolution(pid);
+    } else {
+        std::cerr << "no such type: \"" << type << "\"\n";
+        exit(EXIT_FAILURE);
+    }
+
     benchmark::Initialize(&argc, argv);
-    if (benchmark::ReportUnrecognizedArguments(argc, argv)) 
-        return 1;
+    // if (benchmark::ReportUnrecognizedArguments(argc, argv)) 
+    //     return 1;
     benchmark::RunSpecifiedBenchmarks();                              
     benchmark::Shutdown();
 }
