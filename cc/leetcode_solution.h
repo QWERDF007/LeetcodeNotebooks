5cdc2e3ec4e1346a765a71ab7e98232c29194162
wangtao <595517713@qq.com>
2022-01-30 15:06:56 +0800

    feat: 添加方法 Title、Link，枚举类型 ProblemType

--- cc/leetcode_solution.h
+++ cc/leetcode_solution.h
@@ -5,6 +5,11 @@
 namespace leetcode
 {
 
+enum ProblemType {
+    ARRAY = 0,
+    LINKED_LIST,
+};
+
 enum SolutionsId {
     TWO_SUM = 1,
     ADD_TWO_NUMBERS,
@@ -14,24 +19,38 @@ class LeetcodeSolution
 {
 public:
     /**
+     * @brief leetcode 题目
+     *
+     * @return std::string 题目
+     */
+    virtual std::string Title() = 0;
+
+    /**
      * @brief leetcode 题目问题描述
      * 
      * @return std::string 问题描述
      */
-    virtual std::string problem() = 0;
+    virtual std::string Problem() = 0;
+
+    /**
+     * @brief 链接
+     *
+     * @return 链接
+     */
+    virtual std::string Link() = 0;
 
     /**
      * @brief leetcode 解决方案
      * 
      */
-    virtual void solution() = 0;
+    virtual void Solution() = 0;
 
     /**
      * @brief 评估 solution 的性能
      * 
      * @return std::string 性能报告
      */
-    virtual std::string benchmark() = 0;
+    virtual std::string Benchmark() = 0;
 };
     
 } // namespace leetcode
