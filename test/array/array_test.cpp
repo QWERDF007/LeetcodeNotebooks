32a36b9ef75fc0fa1a395dce168e0e32b86591f7
wangtao <595517713@qq.com>
2022-01-31 22:55:19 +0800

    test(array): 添加两数之和的测试

--- test/array/array_test.cpp
+++ test/array/array_test.cpp
@@ -4,11 +4,39 @@
 namespace leetcode {
 namespace array {
 
-TEST(TwoSumTest, Positive) {
-    TwoSum sol;
-    int target = 9;
-    std::vector<int> inputs({ 2,7,11,15 }), res({ 0, 1 });
-    EXPECT_EQ(sol.Solution1(inputs, target), res);
+TEST(TwoSumTest, TwoSunSolution) {
+    TwoSum solution;
+    
+    std::vector<std::vector<int>> nums({
+        { 2,7,11,15 },
+        { 3,2,4 },
+        { 3,3 },
+        { 2,7,11,15,12,15,67,45,66,11,22,44,978,123,95 },
+    });
+
+    std::vector<int> targets({
+        9,
+        6,
+        6,
+        24,
+    });
+
+    std::vector<std::vector<int>> results({ 
+        { 0,1 },
+        { 1,2 },
+        { 0,1 },
+        { 0,10 },
+    });
+
+    EXPECT_EQ(solution.Solution1(nums[0], targets[0]), results[0]);
+    EXPECT_EQ(solution.Solution1(nums[1], targets[1]), results[1]);
+    EXPECT_EQ(solution.Solution1(nums[2], targets[2]), results[2]);
+    EXPECT_EQ(solution.Solution1(nums[3], targets[3]), results[3]);
+
+    EXPECT_EQ(solution.Solution2(nums[0], targets[0]), results[0]);
+    EXPECT_EQ(solution.Solution2(nums[1], targets[1]), results[1]);
+    EXPECT_EQ(solution.Solution2(nums[2], targets[2]), results[2]);
+    EXPECT_EQ(solution.Solution2(nums[3], targets[3]), results[3]);
 }
 
 };
