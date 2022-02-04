45e0273b1952d20dd2544b94d654b91d1c905c76
wangtao <595517713@qq.com>
2022-02-04 23:09:21 +0800

    test(str): 添加题目二进制求和测试

--- test/str/str_test.cpp
+++ test/str/str_test.cpp
@@ -39,5 +39,35 @@ TEST(RomanToIntTest, LeetcodeTest) {
     EXPECT_EQ(solution.Solution3(strs[3]), results[3]);
 }
 
+TEST(AddBinaryTest, LeetcodeTest) {
+    AddBinary solution;
+
+    std::vector<std::string> a{
+        "11",
+        "1010",
+        "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+    };
+
+    std::vector<std::string> b{
+        "1",
+        "1011",
+        "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011",
+    };
+
+    std::vector<std::string> results{
+        "100",
+        "10101",
+        "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000",
+    };
+
+    EXPECT_EQ(solution.Solution1(a[0], b[0]), results[0]);
+    EXPECT_EQ(solution.Solution1(a[1], b[1]), results[1]);
+    EXPECT_EQ(solution.Solution1(a[2], b[2]), results[2]);
+
+    EXPECT_EQ(solution.Solution2(a[0], b[0]), results[0]);
+    EXPECT_EQ(solution.Solution2(a[1], b[1]), results[1]);
+    EXPECT_EQ(solution.Solution2(a[2], b[2]), results[2]);
+}
+
 } // namespace test
 } // namespace leetcode
