6e1c1c1936a460e69c5e43b7eefe3138964df6b2
wangtao <595517713@qq.com>
2022-02-05 14:31:47 +0800

    test(array): 添加题目区域和检索-数组不可变的测试

--- test/array/array_test.cpp
+++ test/array/array_test.cpp
@@ -99,6 +99,47 @@ TEST(MissingNumberTest, LeetcodeTest) {
     EXPECT_EQ(solution.Solution4(nums[2]), results[2]);
 }
 
+TEST(NumArrayTest, LeetcodeTest) {
+    int n = 1000;
+    std::vector<int> nums(n);
+    for (int i = 0; i < n; ++i) {
+        nums[i] = i + 1;
+    }
+
+    NumArray solution(nums);
+
+    std::vector<int> left{
+        0,
+        0,
+        50,
+        999,
+    };
+
+    std::vector<int> right{
+        0,
+        10,
+        100,
+        999,
+    };
+
+    std::vector<int> results{
+        1,
+        66,
+        3876,
+        1000,
+    };
+
+    EXPECT_EQ(solution.sumRange1(left[0], right[0]), results[0]);
+    EXPECT_EQ(solution.sumRange1(left[1], right[1]), results[1]);
+    EXPECT_EQ(solution.sumRange1(left[2], right[2]), results[2]);
+    EXPECT_EQ(solution.sumRange1(left[3], right[3]), results[3]);
+
+    EXPECT_EQ(solution.sumRange2(left[0], right[0]), results[0]);
+    EXPECT_EQ(solution.sumRange2(left[1], right[1]), results[1]);
+    EXPECT_EQ(solution.sumRange2(left[2], right[2]), results[2]);
+    EXPECT_EQ(solution.sumRange2(left[3], right[3]), results[3]);
+}
+
 } // namespace test
 } // namespace leetcode
 
