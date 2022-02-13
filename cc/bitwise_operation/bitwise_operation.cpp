8fd2d3e407eee4e8987e116a18b2e3bb9b43c6f3
wangtao <595517713@qq.com>
2022-02-13 23:05:42 +0800

    feat(bit): 添加题目位1的个数，解法：右移、左移、N-1

---
+++ cc/bitwise_operation/bitwise_operation.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <benchmark/benchmark.h>
+#include "bitwise_operation.h"
+
+namespace leetcode {
+namespace bit {
+
+void BitwiseOperationSolution(int pid) {
+    LeetcodeSolution *solution = nullptr;
+    switch (pid) {
+        case HAMMING_WEIGHT: solution = new HammingWeight(); break;
+        default: std::cerr << "no such pid: " << pid << std::endl; exit(EXIT_FAILURE); break;
+    }
+    if (solution != nullptr) {
+        std::cout << solution->Title() << std::endl;
+        std::cout << "Link:\n";
+        std::cout << solution->Link() << std::endl << std::endl;
+        std::cout << "Problem:\n";
+        std::cout << solution->Problem() << std::endl;
+        std::cout << "Solution:\n";
+        std::cout << solution->Solution() << std::endl;
+        solution->Benchmark();
+        delete solution;
+    }
+}
+
+std::string HammingWeight::Title() {
+    return "191. 位1的个数\n";
+}
+
+std::string HammingWeight::Problem() {
+    return 
+        "编写一个函数，输入是一个无符号整数（以二进制串的形式），\n"
+        "返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。\n";
+}
+
+std::string HammingWeight::Link() {
+    return "https://leetcode-cn.com/problems/number-of-1-bits/";
+}
+
+std::string HammingWeight::Solution() {
+    return "n-1，时间：O(k)，空间：O(1)。k 为 1 的个数\n";
+}
+
+void HammingWeight::Benchmark() {
+    HammingWeight solution;
+
+    uint32_t n = 0b11111101111011110111110111011101;
+
+    benchmark::RegisterBenchmark("BM_HammingWeight_RightShift", [](benchmark::State &state, HammingWeight solution, uint32_t n) {
+        for (auto _ : state) {
+            solution.Solution1(n);
+        }
+    }, solution, n);
+
+    benchmark::RegisterBenchmark("BM_HammingWeight_LeftShift", [](benchmark::State &state, HammingWeight solution, uint32_t n) {
+        for (auto _ : state) {
+            solution.Solution2(n);
+        }
+    }, solution, n);
+
+    benchmark::RegisterBenchmark("BM_HammingWeight_NSubtractOne", [](benchmark::State &state, HammingWeight solution, uint32_t n) {
+        for (auto _ : state) {
+            solution.Solution3(n);
+        }
+    }, solution, n);
+}
+
+int HammingWeight::Solution1(uint32_t n) {
+    int cnt = 0;
+    while (n > 0) {
+        if (n & 1) {
+            ++cnt;
+        }
+        n = n >> 1;
+    }
+    return cnt;
+}
+
+int HammingWeight::Solution2(uint32_t n) {
+    int cnt = 0;
+    uint64_t i = 1;
+    while (i <= n) {
+        if (n & i) {
+            ++cnt;
+        }
+        i = i << 1;
+    }
+    return cnt;
+}
+
+int HammingWeight::Solution3(uint32_t n) {
+    int cnt = 0;
+    while (n) {
+        ++cnt;
+        n &= n - 1;
+    }
+    return cnt;
+}
+
+} // namespace bit
+} // namespace leetcode
+
+
---
+++ cc/bitwise_operation/bitwise_operation.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstdint>
+#include "leetcode_solution.h"
+
+namespace leetcode {
+namespace bit {
+
+void BitwiseOperationSolution(int pid);
+
+class HammingWeight : public LeetcodeSolution {
+public:
+    std::string Title() override;
+
+    std::string Problem() override;
+
+    std::string Link() override;
+
+    std::string Solution() override;
+
+    void Benchmark() override;
+
+    /**
+     * @brief 左移
+     * 时间复杂度：O(n)，n 为最高位 1 的位置
+     * 空间复杂度：O(1)
+     * @param n 
+     * @return int 
+     */
+    int Solution1(uint32_t n);
+
+    /**
+     * @brief 右移
+     * 时间复杂度：O(n)，n 为最高位 1 的位置
+     * 空间复杂度：O(1)
+     * @param n 
+     * @return int 
+     */
+    int Solution2(uint32_t n);
+
+    /**
+     * @brief n-1
+     * 时间复杂度：O(n)，n 为 1 的个数
+     * 空间复杂度：O(1)
+     * @param n 
+     * @return int 
+     */
+    int Solution3(uint32_t n);
+};
+
+} // namespace bit
+} // namespace leetcode
