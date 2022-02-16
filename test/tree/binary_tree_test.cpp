2405c3d46383e8354bbe3a9071df6fc251011837
wangtao <595517713@qq.com>
2022-02-16 22:54:54 +0800

    test(tree): 添加题目相同的树的测试

--- test/tree/binary_tree_test.cpp
+++ test/tree/binary_tree_test.cpp
@@ -44,5 +44,47 @@ TEST(BinaryTreeTest, InorderTraversalTest) {
     }
 }
 
+TEST(BinaryTreeTest, IsSameTreeTest) {
+    IsSameTree solution;
+
+    std::vector<std::vector<std::string>> sp{
+        { "1","2","3" },
+        { "1","2" },
+        { "1","2","1" },
+    };
+
+    std::vector<std::vector<std::string>> sq{
+        { "1","2","3" },
+        { "1","null","2" },
+        { "1","1","2" },
+    };
+
+    std::vector<TreeNode *> p, q;
+
+    for (auto &s : sp) {
+        TreeNode *root = NewTree(s);
+        p.emplace_back(root);
+    }
+    for (auto &s : sq) {
+        TreeNode *root = NewTree(s);
+        q.emplace_back(root);
+    }
+
+    EXPECT_TRUE(solution.Solution1(p[0], q[0]));
+    EXPECT_FALSE(solution.Solution1(p[1], q[1]));
+    EXPECT_FALSE(solution.Solution1(p[2], q[2]));
+
+    EXPECT_TRUE(solution.Solution2(p[0], q[0]));
+    EXPECT_FALSE(solution.Solution2(p[1], q[1]));
+    EXPECT_FALSE(solution.Solution2(p[2], q[2]));
+
+    for (auto root : p) {
+        DeleteTree(root);
+    }
+    for (auto root : q) {
+        DeleteTree(root);
+    }
+}
+
 } // namespace test
 } // namespace leetcode
