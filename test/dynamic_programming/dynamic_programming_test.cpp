509ad61e9a7aec6a4207ffa52d601dcbca9f9338
wangtao <595517713@qq.com>
2022-02-17 14:28:06 +0800

    test(dp): 添加题目骑士在棋盘上的概率的测试

---
+++ test/dynamic_programming/dynamic_programming_test.cpp
@@ -0,0 +1,17 @@
+﻿#include <gtest/gtest.h>
+#include "dynamic_programming/dynamic_programming.h"
+
+namespace leetcode {
+namespace test {
+
+using namespace dp;
+
+TEST(DynamicProgrammingTest, KnightProbabilityTest) {
+    KnightProbability solution;
+
+    EXPECT_EQ(solution.Solution1(3, 2, 0, 0), 0.0625);
+    EXPECT_EQ(solution.Solution1(1, 0, 0, 0), 1.00000);
+}
+
+} // namespace test
+} // namespace leetcode
