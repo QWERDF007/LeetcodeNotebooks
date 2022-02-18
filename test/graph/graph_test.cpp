d80e9b6739749f4506bbd54cfc5d14c4c95a2c91
wangtao <595517713@qq.com>
2022-02-18 10:24:59 +0800

    test(graph): 添加题目找出星型图的中心节点的测试

---
+++ test/graph/graph_test.cpp
@@ -0,0 +1,28 @@
+﻿#include <gtest/gtest.h>
+#include "graph/graph.h"
+
+namespace leetcode {
+namespace test {
+
+using namespace graph;
+
+TEST(GraphTest, FindCenterTest) {
+    FindCenter solution;
+
+    std::vector<std::vector<std::vector<int>>> edges{
+        { {1,2},{2,3},{4,2} },
+        { {1,2},{5,1},{1,3},{1,4} },
+    };
+
+    EXPECT_EQ(solution.Solution1(edges[0]), 2);
+    EXPECT_EQ(solution.Solution1(edges[1]), 1);
+
+    EXPECT_EQ(solution.Solution2(edges[0]), 2);
+    EXPECT_EQ(solution.Solution2(edges[1]), 1);
+
+    EXPECT_EQ(solution.Solution3(edges[0]), 2);
+    EXPECT_EQ(solution.Solution3(edges[1]), 1);
+}
+
+} // namespace test
+} // namespace leetcode
