2f9d4f6b62da1f9ed9bb70a7400c7321f31fca6e
wangtao <595517713@qq.com>
2022-02-19 15:56:14 +0800

    test(greedy): 添加题目煎饼排序的测试，比较经过煎饼排序后的数组，比较煎饼次数是否超过上限

--- test/greedy_algorithm/greedy_algorithm_test.cpp
+++ test/greedy_algorithm/greedy_algorithm_test.cpp
@@ -26,5 +26,37 @@ TEST(GreedyAlgorithmTest, FindContentChildrenTest) {
     EXPECT_EQ(solution.Solution1(g[1], s[1]), targets[1]);
 }
 
+TEST(GreedyAlgorithmTest, PancakeSortTest) {
+    PancakeSort solution;
+
+    std::vector<std::vector<int>> arrs{
+        { 3,2,4,1 },
+        { 1,2,3 },
+    };
+
+    std::vector<std::vector<int>> arrs_to_pancake = arrs;
+
+    std::vector<std::vector<int>> results{
+        { 1,2,3,4 },
+        { 1,2,3 },
+    };
+
+    std::vector<std::vector<int>> panake_indices;
+    panake_indices.emplace_back(solution.Solution1(arrs[0]));
+    panake_indices.emplace_back(solution.Solution1(arrs[1]));
+
+    std::vector<int> pancake_times(arrs.size());
+    pancake_times[0] = panake_indices[0].size();
+    pancake_times[1] = panake_indices[1].size();
+
+    solution.Pancake(arrs_to_pancake[0], panake_indices[0]);
+    solution.Pancake(arrs_to_pancake[1], panake_indices[1]);
+
+    EXPECT_EQ(arrs_to_pancake[0], results[0]);
+    EXPECT_EQ(arrs_to_pancake[1], results[1]);
+    EXPECT_LE(pancake_times[0], 10 * arrs[0].size());
+    EXPECT_LE(pancake_times[1], 10 * arrs[1].size());
+}
+
 } // namespace test
 } // namespace leetcode
