48555bb4ad1996e6ced6cd957707e8da7fb19eeb
wangtao <595517713@qq.com>
2022-02-21 21:18:00 +0800

    docs(tp): 添加函数注释

--- cc/two_pointers/two_pointers.h
+++ cc/two_pointers/two_pointers.h
@@ -20,8 +20,22 @@ public:
 
     void Benchmark() override;
 
+    /**
+     * @brief 双指针模拟
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param dominoes 多米诺骨牌的初始状态
+     * @return std::string 多米诺骨牌的最终状态
+     */
     std::string Solution1(std::string dominoes);
-
+    
+    /**
+     * @brief 广度优先搜索 (BFS)
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     * @param dominoes 多米诺骨牌的初始状态
+     * @return std::string 多米诺骨牌的最终状态
+     */
     std::string Solution2(std::string dominoes);
 };
 
