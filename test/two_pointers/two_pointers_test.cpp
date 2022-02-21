1b37a2a0d98e3537bfef01fd93bf01cb7150c51e
wangtao <595517713@qq.com>
2022-02-21 14:10:20 +0800

    test(tp): 添加题目推多米诺的测试

---
+++ test/two_pointers/two_pointers_test.cpp
@@ -0,0 +1,22 @@
+﻿#include <gtest/gtest.h>
+#include "two_pointers/two_pointers.h"
+
+namespace leetcode {
+namespace test {
+
+using namespace tp;
+
+TEST(TwoPointersTest, PushDominoesTest) {
+    PushDominoes solution;
+
+    EXPECT_EQ(solution.Solution1("RR..LL"), "RRRLLL");
+    EXPECT_EQ(solution.Solution1("RR.L"), "RR.L");
+    EXPECT_EQ(solution.Solution1(".L.R...LR..L.."), "LL.RR.LLRRLL..");
+
+    EXPECT_EQ(solution.Solution2("RR..LL"), "RRRLLL");
+    EXPECT_EQ(solution.Solution2("RR.L"), "RR.L");
+    EXPECT_EQ(solution.Solution2(".L.R...LR..L.."), "LL.RR.LLRRLL..");
+}
+
+} // namespace test
+} // namespace leetcode
