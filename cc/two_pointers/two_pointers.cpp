1fc3c82b61fbb5ad02d76c65a876b382038ee148
wangtao <595517713@qq.com>
2022-02-23 09:19:20 +0800

    feat(tp): 添加题目仅仅反转字母，解法：双指针

--- cc/two_pointers/two_pointers.cpp
+++ cc/two_pointers/two_pointers.cpp
@@ -11,6 +11,7 @@ void TwoPointersSolution(int pid) {
     LeetcodeSolution *solution = nullptr;
     switch (pid) {
         case SolutionsId::PUSH_DOMINOES: solution = new PushDominoes(); break;
+        case SolutionsId::REVERSE_ONLY_LETTERS: solution = new ReverseOnlyLetters(); break;
         default: std::cerr << "no such pid: " << pid << std::endl; exit(EXIT_FAILURE); break;
     }
     if (solution != nullptr) {
@@ -144,6 +145,92 @@ std::string PushDominoes::Solution2(std::string dominoes) {
         }
     }
     return res;
+}
+
+std::string ReverseOnlyLetters::Title() {
+    return "917. 仅仅反转字母\n";
+}
+
+std::string ReverseOnlyLetters::Problem() {
+    return 
+        "给你一个字符串 s，根据下述规则反转字符串：\n"
+        "- 所有非英文字母保留在原有位置。\n"
+        "- 所有英文字母（小写或大写）位置反转。\n"
+        "返回反转后的 s。\n";
+}
+
+std::string ReverseOnlyLetters::Link() {
+    return "https://leetcode-cn.com/problems/reverse-only-letters/";
+}
+
+std::string ReverseOnlyLetters::Solution() {
+    return "双指针，时间：O(n)，空间：O(1)。\n";
+}
+
+void ReverseOnlyLetters::Benchmark() {
+    ReverseOnlyLetters solution;
+
+    int n = 1000;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(33, 122);
+    std::string s;
+    for (int i = 0; i < n; ++i) {
+        s += dis(gen);
+    }
+
+    benchmark::RegisterBenchmark("BM_ReverseOnlyLetters_TwoPointers", [](benchmark::State &state, ReverseOnlyLetters solution, std::string s) {
+        for (auto _ : state) {
+            solution.Solution1(s);
+        }
+    }, solution, s);
+
+    benchmark::RegisterBenchmark("BM_ReverseOnlyLetters_TwoPointersInPlace", [](benchmark::State &state, ReverseOnlyLetters solution, std::string s) {
+        for (auto _ : state) {
+            solution.Solution2(s);
+        }
+    }, solution, s);
+}
+
+std::string ReverseOnlyLetters::Solution1(std::string s) {
+    int i = 0, n = s.size(), j = n - 1;
+    std::string ans(n, ' ');
+    while (i < n) {
+        // 字母
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+            while (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a')) {
+                ans[j] = s[j];
+                --j;
+            }
+            ans[j] = s[i];
+            --j;
+        }
+        else {
+            ans[i] = s[i];
+        }
+        ++i;
+    }
+    return ans;
+}
+
+std::string ReverseOnlyLetters::Solution2(std::string s) {
+    int n = s.size();
+    int left = 0, right = n - 1;
+    while (true) {
+        while (left < right && !std::isalpha(s[left])) {
+            ++left;
+        }
+        while (right > left && !std::isalpha(s[right])) {
+            --right;
+        }
+        if (left >= right) {
+            break;
+        }
+        std::swap(s[left], s[right]);
+        ++left;
+        --right;
+    }
+    return s;
 }
 
 } // namespace tp    
--- cc/two_pointers/two_pointers.h
+++ cc/two_pointers/two_pointers.h
@@ -39,5 +39,36 @@ public:
     std::string Solution2(std::string dominoes);
 };
 
+class ReverseOnlyLetters : public LeetcodeSolution {
+public:
+    std::string Title() override;
+
+    std::string Problem() override;
+
+    std::string Link() override;
+
+    std::string Solution() override;
+
+    void Benchmark() override;
+
+    /**
+     * @brief 双指针 (就地)
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param s 待反转的字符串
+     * @return std::string 反转后的字符串
+     */
+    std::string Solution1(std::string s);
+
+    /**
+     * @brief 双指针
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     * @param s 待反转的字符串
+     * @return std::string 反转后的字符串
+     */
+    std::string Solution2(std::string s);
+};
+
 } // namespace tp
 } // namespace leetcode
