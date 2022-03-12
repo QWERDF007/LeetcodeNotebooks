55f23ffadb41c4757606d373accf81a63572280a
wangtao <595517713@qq.com>
2022-03-12 17:39:43 +0800

    feat(main): 添加pid到type的映射，添加cout的对pid和type重载，删除cxxopts参数type，通过pid解析type，添加参数all，运行全部题目

--- cc/leetcode_solution.h
+++ cc/leetcode_solution.h
@@ -1,6 +1,9 @@
-#pragma once
+﻿#pragma once
 
+#include <iostream>
 #include <string>
+#include <map>
+#include <unordered_map>
 
 namespace leetcode {
 
@@ -45,7 +48,7 @@ public:
  * @brief 问题类型
  * 
  */
-enum ProblemType {
+enum class ProblemType {
     ARRAY = 1,
     STR = 2,
     DYNAMIC_PROGRAMMING = 4,
@@ -53,16 +56,33 @@ enum ProblemType {
     GREEDY_ALGORITHM,
     LINKED_LIST,
     BITWISE_OPERATION,
-    BINARY_TREE,
+    TREE,
     TWO_POINTERS,
     GRAPH,
+};
+
+/**
+ * @brief ProblemType 到字符串的映射
+ * 
+ */
+const std::map<ProblemType, std::string> kTypeToStr{
+    { ProblemType::ARRAY, "ARRAY" },
+    { ProblemType::STR, "STR" },
+    { ProblemType::DYNAMIC_PROGRAMMING, "DYNAMIC_PROGRAMMING" },
+    { ProblemType::MATH, "MATH" },
+    { ProblemType::GREEDY_ALGORITHM, "GREEDY_ALGORITHM" },
+    { ProblemType::LINKED_LIST, "LINKED_LIST" },
+    { ProblemType::BITWISE_OPERATION, "BITWISE_OPERATION" },
+    { ProblemType::TREE, "TREE" },
+    { ProblemType::TWO_POINTERS, "TWO_POINTERS" },
+    { ProblemType::GRAPH, "GRAPH" },
 };
 
 /**
  * @brief 题目号，对应leetcode上的题目
  * 
  */
-enum SolutionsId {
+enum class SolutionsId {
     TWO_SUM = 1,
     ADD_TWO_NUMBERS = 2,
     IS_PALINDROME = 9,
@@ -122,7 +142,97 @@ enum SolutionsId {
     MAXIMUM_DIFFERENCE = 2016,
     PLATES_BETWEEN_CANDLES = 2055,
 };
-    
+
+/**
+ * @brief pid 到 type 的映射
+ * 
+ */
+const std::map<SolutionsId, ProblemType> kPidToType{
+    { SolutionsId::TWO_SUM, ProblemType::ARRAY },
+    { SolutionsId::ADD_TWO_NUMBERS, ProblemType::LINKED_LIST },
+    { SolutionsId::IS_PALINDROME, ProblemType::MATH },
+    { SolutionsId::ROMAN_TO_INT, ProblemType::STR },
+    { SolutionsId::REMOVE_DUPLICATES, ProblemType::ARRAY },
+    { SolutionsId::ADD_BINARY, ProblemType::STR },
+    { SolutionsId::MY_SQRT, ProblemType::MATH },
+    { SolutionsId::INORDER_TRAVERSAL, ProblemType::TREE },
+    { SolutionsId::IS_SAME_TREE, ProblemType::TREE },
+    { SolutionsId::IS_BALANCED, ProblemType::TREE },
+    { SolutionsId::MIN_DEPTH, ProblemType::TREE },
+    { SolutionsId::HAS_PATH_SUM, ProblemType::TREE },
+    { SolutionsId::PREORDER_TRAVERSAL, ProblemType::TREE },
+    { SolutionsId::POSTORDER_TRAVERSAL, ProblemType::TREE },
+    { SolutionsId::UPSIDE_DOWN_BINARY_TREE, ProblemType::TREE },
+    { SolutionsId::FIND_MISSING_RANGES, ProblemType::ARRAY },
+    { SolutionsId::CONVERT_TO_TITLE, ProblemType::STR },
+    { SolutionsId::HAMMING_WEIGHT, ProblemType::BITWISE_OPERATION },
+    { SolutionsId::IS_HAPPY, ProblemType::MATH },
+    { SolutionsId::INVERT_TREE, ProblemType::TREE },
+    { SolutionsId::LOWEST_COMMON_ANCESTOR, ProblemType::TREE },
+    { SolutionsId::BINARY_TREE_PATHS, ProblemType::TREE },
+    { SolutionsId::ADD_DIGITS, ProblemType::MATH },
+    { SolutionsId::MISSING_NUMBER, ProblemType::ARRAY },
+    { SolutionsId::NUM_ARRAY, ProblemType::ARRAY },
+    { SolutionsId::INTERSECTION, ProblemType::ARRAY },
+    { SolutionsId::INTERSECT, ProblemType::ARRAY },
+    { SolutionsId::SUM_OF_LEFT_LEAVES, ProblemType::TREE },
+    { SolutionsId::LONGEST_PALINDROME, ProblemType::STR },
+    { SolutionsId::THIRD_MAX, ProblemType::ARRAY },
+    { SolutionsId::FIND_DISAPPEARED_NUMBERS, ProblemType::ARRAY },
+    { SolutionsId::MIN_MOVES, ProblemType::ARRAY },
+    { SolutionsId::FIND_CONTENT_CHILDREN, ProblemType::GREEDY_ALGORITHM },
+    { SolutionsId::FIND_MODE, ProblemType::TREE },
+    { SolutionsId::CONVERT_TO_BASE_7, ProblemType::MATH },
+    { SolutionsId::SINGLE_NON_DUPLICATE, ProblemType::ARRAY },
+    { SolutionsId::COMPLEX_NUMBER_MULTIPLY, ProblemType::MATH },
+    { SolutionsId::FIND_TILT, ProblemType::TREE },
+    { SolutionsId::IS_SUBTREE, ProblemType::TREE },
+    { SolutionsId::PREORDER, ProblemType::TREE },
+    { SolutionsId::POSTORDER, ProblemType::TREE },
+    { SolutionsId::TREE_2_STR, ProblemType::TREE },
+    { SolutionsId::AVERAGE_OF_LEVELS, ProblemType::TREE },
+    { SolutionsId::KNIGHT_PROBABILITY, ProblemType::DYNAMIC_PROGRAMMING },
+    { SolutionsId::IS_ONE_BIT_CHARACTER, ProblemType::ARRAY },
+    { SolutionsId::PUSH_DOMINOES, ProblemType::TWO_POINTERS },
+    { SolutionsId::REVERSE_ONLY_LETTERS, ProblemType::TWO_POINTERS },
+    { SolutionsId::PANCAKE_SORT, ProblemType::GREEDY_ALGORITHM },
+    { SolutionsId::COMMON_CHARS, ProblemType::STR },
+    { SolutionsId::MAX_NUMBER_OF_BALLONS, ProblemType::STR },
+    { SolutionsId::LUCKY_NUMBERS, ProblemType::ARRAY },
+    { SolutionsId::MAX_POWER, ProblemType::STR },
+    { SolutionsId::FIND_BALL, ProblemType::ARRAY },
+    { SolutionsId::FIND_CENTER, ProblemType::GRAPH },
+    { SolutionsId::SECOND_HIGHEST, ProblemType::STR },
+    { SolutionsId::MINIMUM_DIFFERENCE, ProblemType::ARRAY },
+    { SolutionsId::MAXIMUM_DIFFERENCE, ProblemType::ARRAY },
+    { SolutionsId::PLATES_BETWEEN_CANDLES, ProblemType::STR },
+};
+
+/**
+ * @brief 重载 cout，输出 SolutionsId
+ * 利用 inline 修饰，可以避免头文件被多重引用时的重复定义
+ * @param os 输出流
+ * @param obj SolutionsId 枚举类
+ * @return std::ostream& 输出流 
+ */
+inline std::ostream &operator << (std::ostream &os, const SolutionsId obj) {
+    os << static_cast<std::underlying_type<SolutionsId>::type>(obj);
+    return os;
+}
+
+/**
+ * @brief 重载 cout，输出 ProblemType
+ * 利用 inline 修饰，可以避免头文件被多重引用时的重复定义
+ * @param os 输出流
+ * @param obj ProblemType 枚举类
+ * @return std::ostream& 输出流
+ */
+inline std::ostream &operator << (std::ostream &os, const ProblemType obj) {
+    os << static_cast<std::underlying_type<ProblemType>::type>(obj);
+    return os;
+}
+
+
 } // namespace leetcode
 
 
--- main.cpp
+++ main.cpp
@@ -16,41 +16,92 @@
 #include "graph/graph.h"
 #include "two_pointers/two_pointers.h"
 
+using namespace leetcode;
 
+void run(ProblemType type, SolutionsId pid) {
+    switch (type) {
+        case ProblemType::ARRAY: leetcode::array::ArraySolution(pid); break;
+        case ProblemType::STR:  leetcode::str::StrSolution(pid); break;
+        case ProblemType::DYNAMIC_PROGRAMMING: leetcode::dp::DynamicProgrammingSolution(pid); break;
+        case ProblemType::MATH: leetcode::math::MathSolution(pid); break;
+        case ProblemType::GREEDY_ALGORITHM: leetcode::greedy::GreedyAlgorithmSolution(pid); break;
+        case ProblemType::LINKED_LIST: leetcode::linkedlist::LinkedListSolution(pid); break;
+        case ProblemType::BITWISE_OPERATION: leetcode::bit::BitwiseOperationSolution(pid); break;
+        case ProblemType::TREE: leetcode::tree::TreeSolution(pid); break;
+        case ProblemType::TWO_POINTERS: leetcode::tp::TwoPointersSolution(pid); break;
+        case ProblemType::GRAPH: leetcode::graph::GraphSolution(pid); break;
+        default: std::cerr << "no such type: " << type << std::endl; exit(EXIT_FAILURE); break;
+    }
+}
 
 int main(int argc, char* argv[]) {
     try {
         cxxopts::Options options(argv[0], "Leetcode C++ Solutions");
         options.allow_unrecognised_options();
         options.add_options()
-            ("t,type", "problem type", cxxopts::value<int>())
+            ("l,list", "list all finished pid", cxxopts::value<bool>()->default_value("false"))
             ("i,pid", "problem id", cxxopts::value<int>())
+            ("a,all", "run all finished problem", cxxopts::value<bool>()->default_value("false"))
             ("h,help", "Print usage");
         cxxopts::ParseResult opts = options.parse(argc, argv);
         if (opts.count("help")) {
-            std::cout << options.help() << std::endl;
+            std::cout << options.help() << std::endl << std::endl;
+            exit(EXIT_SUCCESS);
+        }
+        
+        if (opts.count("list")) {
+            int total = 0;
+            std::map<ProblemType, std::vector<SolutionsId>> counts;
+            for (auto &[pid, type] : kPidToType) {
+                if (counts.count(type)) {
+                    counts[type].emplace_back(pid);
+                }
+                else {
+                    counts[type] = std::vector<SolutionsId>{ pid };
+                }
+                ++total;
+            }
+
+            std::cout << "total: " << total << std::endl << std::endl;
+            for (auto &[type, pids] : counts) {
+                auto finded = kTypeToStr.find(type);
+                if (finded != kTypeToStr.end()) {
+                    std::cout << finded->second << " (" << pids.size() << "):\n";
+                    for (auto pid : pids) {
+                        std::cout << static_cast<int>(pid) << " ";
+                    }
+                    std::cout << std::endl << std::endl;
+                }
+            }
             exit(EXIT_SUCCESS);
         }
 
-        int type = -1, pid = -1;
-        if (opts.count("type")) {
-            type = opts["type"].as<int>();
-        }
-        if (opts.count("pid")) {
-            pid = opts["pid"].as<int>();
-        }
-        switch (type) {
-            case leetcode::ARRAY: leetcode::array::ArraySolution(pid); break;
-            case leetcode::STR:  leetcode::str::StrSolution(pid); break;
-            case leetcode::DYNAMIC_PROGRAMMING: leetcode::dp::DynamicProgrammingSolution(pid); break;
-            case leetcode::MATH: leetcode::math::MathSolution(pid); break;
-            case leetcode::GREEDY_ALGORITHM: leetcode::greedy::GreedyAlgorithmSolution(pid); break;
-            case leetcode::LINKED_LIST: leetcode::linkedlist::LinkedListSolution(pid); break;
-            case leetcode::BITWISE_OPERATION: leetcode::bit::BitwiseOperationSolution(pid); break;
-            case leetcode::BINARY_TREE: leetcode::tree::TreeSolution(pid); break;
-            case leetcode::TWO_POINTERS: leetcode::tp::TwoPointersSolution(pid); break;
-            case leetcode::GRAPH: leetcode::graph::GraphSolution(pid); break;
-            default: std::cerr << "no such type: \"" << type << "\"\n"; exit(EXIT_FAILURE); break;
+
+
+        if (opts.count("all")) {
+            int total = 0;
+            for (auto &[pid, type] : kPidToType) {
+                run(type, pid);
+                ++total;
+            }
+            std::cout << total << " finished leetcode problems!\n\n";
+        } else {
+            ProblemType type = ProblemType(-1);
+            SolutionsId pid = SolutionsId(-1);
+
+            if (opts.count("pid")) {
+                pid = SolutionsId(opts["pid"].as<int>());
+            }
+
+            auto finded = kPidToType.find(SolutionsId(pid));
+            if (finded != kPidToType.end()) {
+                type = finded->second;
+            } else {
+                std::cerr << "no such pid: " << pid << std::endl; 
+                std::cerr << "you can use \"" << argv[0] << " -l\" to list all finished pid!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+            run(type, pid);
         }
 
         benchmark::Initialize(&argc, argv);
