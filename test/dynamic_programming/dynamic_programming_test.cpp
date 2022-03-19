9163ebf6193b4c5270a82acc473411b37cf9dfe7
wangtao <595517713@qq.com>
2022-03-19 15:25:16 +0800

    test(dp): 添加题目不同的二叉搜索树的测试

--- test/dynamic_programming/dynamic_programming_test.cpp
+++ test/dynamic_programming/dynamic_programming_test.cpp
@@ -6,6 +6,25 @@ namespace test {
 
 using namespace dp;
 
+TEST(DynamicProgrammingTest, NumTreesTest) {
+    NumTrees solution;
+    std::vector<int> n{
+        3, 1, 19
+    };
+
+    std::vector<int> results{
+        5, 1, 1767263190,
+    };
+
+    EXPECT_EQ(solution.Solution1(n[0]), results[0]);
+    EXPECT_EQ(solution.Solution1(n[1]), results[1]);
+    EXPECT_EQ(solution.Solution1(n[2]), results[2]);
+
+    EXPECT_EQ(solution.Solution2(n[0]), results[0]);
+    EXPECT_EQ(solution.Solution2(n[1]), results[1]);
+    EXPECT_EQ(solution.Solution2(n[2]), results[2]);
+}
+
 TEST(DynamicProgrammingTest, KnightProbabilityTest) {
     KnightProbability solution;
 
